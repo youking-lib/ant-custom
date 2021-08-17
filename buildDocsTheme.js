7f76da3f4e01e24bc68ae7e7a8ac2ce649ff39b1
yongzhen <yongzhen@kuaishou.com>
2021-08-18 00:06:17 +0800

    feat: theme

--- buildDocsTheme.js
+++ buildDocsTheme.js
@@ -9,16 +9,27 @@ if (fs.existsSync('./docs-theme')) {
 fs.mkdirSync('./docs-theme');
 
 const cpaths = fs.readdirSync('./components').filter(item => {
-  return !['__tests__', '_util'].includes(item);
+  return !['__tests__', '_util', 'index.tsx'].includes(item);
 });
 
 cpaths.forEach(item => {
-  fs.mkdirSync(`./docs-theme/${item}`);
-  
   const src = `components/${item}/style`;
   const dist = `./docs-theme/${item}/style`;
   const origin_dist = `./docs-theme/${item}/_style`;
-  
-  child_process.spawn('cp', ['-r', src, dist]);
-  child_process.spawn('cp', ['-r', src, origin_dist]);	
+
+  if (fs.existsSync(src)) {
+    fs.mkdirSync(`./docs-theme/${item}`);
+    sync(src, dist, origin_dist);
+  }
 })
+
+sync(`components/style`, './docs-theme/style');
+
+function sync (src, dist, origin_dist) {
+  
+    child_process.spawn('cp', ['-r', src, dist]);
+
+    if (origin_dist) {
+      child_process.spawn('cp', ['-r', src, origin_dist]);
+    }
+}
