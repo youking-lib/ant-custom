c9926cf396d296e6b59098f5bf4a70fe31eb44ff
yongzhen <yongzhen@kuaishou.com>
2021-09-28 18:48:09 +0800

    feat: discussion

--- buildDocsTheme.js
+++ buildDocsTheme.js
@@ -12,14 +12,22 @@ const cpaths = fs.readdirSync('./components').filter(item => {
   return !['__tests__', '_util', 'index.tsx'].includes(item);
 });
 
+// @import './affix/style/index.less';
+let indexLessContent = '';
+
 cpaths.forEach(item => {
   const src = `components/${item}/style`;
   const dist = `./docs-theme/${item}/style`;
   const origin_dist = `./docs-theme/${item}/_style`;
 
+  
   if (fs.existsSync(src)) {
     fs.mkdirSync(`./docs-theme/${item}`);
     sync(src, dist, origin_dist);
+    
+    if (fs.existsSync(`${src}/index.less`)) {
+      indexLessContent += `@import './${item}/style/index.less';\n`;
+    }
   }
 })
 
@@ -33,3 +41,7 @@ function sync (src, dist, origin_dist) {
       child_process.spawn('cp', ['-r', src, origin_dist]);
     }
 }
+
+fs.writeFileSync('./docs-theme/index.less', indexLessContent, {
+  encoding: 'utf8'
+});
